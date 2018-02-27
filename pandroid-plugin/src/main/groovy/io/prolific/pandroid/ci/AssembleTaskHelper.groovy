c44692e8dc3364951aab6476ff6bc026fc441239
Chahine <chahine.m@hotmail.com>
2018-02-27 15:21:14 -0500

    Feature - Rename Apk

--- pandroid-plugin/src/main/groovy/io/prolific/pandroid/ci/AssembleTaskHelper.groovy
+++ pandroid-plugin/src/main/groovy/io/prolific/pandroid/ci/AssembleTaskHelper.groovy
@@ -17,4 +17,17 @@ final class AssembleTaskHelper {
     def variant = matcher.group(2)
     return "${projectName}/build/outputs/apk${variant.replaceAll("([A-Z])", "/\$1").toLowerCase()}/"
   }
+
+  static void renameApk(File ciFolder, outputApkFile) {
+    def outputFile = new File("${ciFolder.path}/output.json")
+    outputFile.withReader {
+      it.eachLine {
+        def matcher = Pattern.compile("\"path\":\"(.*).apk\"").matcher(it)
+        if (matcher.find()) {
+          new File("${ciFolder.path}/${matcher.group(1)}.apk").renameTo(
+              "${ciFolder.path}/$outputApkFile")
+        }
+      }
+    }
+  }
 }
