364e601ae34c74ca485de2067dd43b83349aeb1e
Chahine <chahine.m@hotmail.com>
2018-04-09 14:05:55 -0400

    Feature - Proper Task Dependencies

--- pandroid-plugin/src/main/groovy/io/prolific/pandroid/keiko/KeikoTask.groovy
+++ pandroid-plugin/src/main/groovy/io/prolific/pandroid/keiko/KeikoTask.groovy
@@ -1,7 +1,6 @@
 package io.prolific.pandroid.keiko
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
 
 class KeikoTask extends DefaultTask {
@@ -10,12 +9,5 @@ class KeikoTask extends DefaultTask {
     description = 'Runs all keiko related tasks'
   }
 
-  @TaskAction def keiko() {
-    if (!project.plugins.hasPlugin("org.sonarqube")) {
-      throw new GradleException(
-          "Failed running :keiko on a project that does not have the `org.sonarqube` plugin applied to it")
-    }
-    def process = "./gradlew sonarqube".execute()
-    process.waitForProcessOutput(System.out, System.err)
-  }
+  @TaskAction def keiko() {}
 }
